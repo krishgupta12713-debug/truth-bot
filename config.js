15094fcf0463db0952a080ecf2ee80c1932458ec
krishgupta12713-debug <krishgupta12713@gmail.com>
2025-12-06 12:17:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    prefix: '!',
+    nodes: [{
+        host: "lavalink.jirayu.net",
+        password: "youshallnotpass",
+        port: 13592,
+        secure: false,
+        name: "Main Node"
+    }],
+    spotify: {
+        clientId: "a568b55af1d940aca52ea8fe02f0d93b",
+        clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789"
+    },
+    botToken: "MTQ0NjcyNjI3Mjk3MjYxOTc3Ng.G_cKX1.OzIyWUl44MfAOKSYPyVUSN_VaV5XFw1V3-f1FA",
+    embedColor: "#0061ff"
+}
